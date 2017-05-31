aff77312406d0fbf39bf95850b61ffa85a99a51e
giuliano <giuliano.sider@gmail.com>
2017-05-30 22:14:10 -0300

    nome melhor pra pasta

---
+++ Prototipo-Servidor-NodeJS/firstProgram.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var ocupada = false;
+
+var EventEmitter = require('events').EventEmitter;
+var emitter = new EventEmitter();
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/waiting', (req, res) => {
+    res.sendFile(path.join(__dirname+'/waiting.html'));
+});
+
+app.get('/welcome', (req, res) => {
+    res.sendFile(path.join(__dirname+'/welcome.html'));
+});
+
+app.post('/server', (req, res) => {
+    emitter.on('server', function refreshHandler () {
+      console.log('evento');
+      emitter.removeListener('server', refreshHandler);
+      res.send('ok');
+    })
+});
+
+app.post('/*', function (req, res) {
+  console.log(req.url);
+  ocupada = !ocupada;
+  emitter.emit('server', {});
+  res.send('Got a POST request');
+});
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+});
