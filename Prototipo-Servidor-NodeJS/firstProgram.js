cd48a916e4fb576b99a9919330500dce548f9810
Lucas Brito <lucasbfmatos@hotmail.com>
2017-06-20 21:20:10 -0300

    treinos adicionados ao servidor

--- Prototipo-Servidor-NodeJS/firstProgram.js
+++ Prototipo-Servidor-NodeJS/firstProgram.js
@@ -21,14 +21,14 @@ var emitter = new EventEmitter();
 // estado da(s) High Tech Pool(s):
 
 // a raia está ocupada? inicialmente não
-var ocupada = false; 
+var ocupada = false;
 
 
 // vai servir estaticamente tudo nessa pasta ...
 app.use(express.static(__dirname + '/'));
 
 // página de espera da raia
-app.get('/waiting', (req, res) => { 
+app.get('/waiting', (req, res) => {
     res.sendFile(path.join(__dirname+'/waiting.html'));
 });
 
@@ -37,31 +37,51 @@ app.get('/welcome', (req, res) => {
     res.sendFile(path.join(__dirname+'/welcome.html'));
 });
 
-// páginas waiting e welcome postam aqui 
+app.get('/treino_livre', (req, res) => {
+    res.sendFile(path.join(__dirname+'/treino-livre.html'));
+});
+
+app.get('/escolha_treino', (req, res) => {
+    res.sendFile(path.join(__dirname+'/escolha-treino.html'));
+});
+
+app.get('/treinamentoA', (req, res) => {
+    res.sendFile(path.join(__dirname+'/treinamentoA.html'));
+});
+
+app.get('/treinamentoB', (req, res) => {
+    res.sendFile(path.join(__dirname+'/treinamentoB.html'));
+});
+
+app.get('/treinamentoC', (req, res) => {
+    res.sendFile(path.join(__dirname+'/treinamentoC.html'));
+});
+
+// páginas waiting e welcome postam aqui
 // para esperar o evento de check in e check out de raia
 // PROBLEMA: acho que esse request dá timeout depois de um tempo
 // SOLUÇÃO: web sockets?
 app.post('/esperando_toggle_cartao', (req, res) => {
     emitter.on('toggle_cartao', function refreshHandler () {
       console.log('evento');
-      emitter.removeListener('server', refreshHandler);
+      emitter.removeListener('toggle_cartao', refreshHandler);
       res.send('ok');
     })
 });
 
 // list of  all the sql queries we need
-queries = [
-    cabecalho_tipo_treinamento: 
+/*queries = [
+    cabecalho_tipo_treinamento:
         () => 'select * from Tipo_Treinamento;',
-    
-    sequencia_exercicios_em_tipo_treinamento: 
+
+    sequencia_exercicios_em_tipo_treinamento:
         (id_tipo_treinamento) => 'select te.*' +
                                  'from Tipo_Exercicio as te' +
-                                 '     join Sequencia_Treinamento as st' + 
+                                 '     join Sequencia_Treinamento as st' +
                                  '          on te.id = st.id_tipo_exercicio' +
                                  '     join Tipo_Treinamento as tt' +
                                  '          on tt.id = st.id_tipo_treinamento' +
-                                 'where tt.id = ' + id_tipo_treinamento + ' ' + 
+                                 'where tt.id = ' + id_tipo_treinamento + ' ' +
                                  'order by st.numero_de_sequencia;',
 
 
@@ -69,9 +89,9 @@ queries = [
 
 // retorna uma lista (JSON) de treinamentos disponiveis do BD High Tech Pool
 app.get('./treinamentos_disponiveis', (req, res) => {
-    
+
     connection.connect(function(err) {
-// PYRAMID OF DOOM. FIX??  
+// PYRAMID OF DOOM. FIX??
         if (err) throw err;
         connection.query(
             queries['cabecalho_tipo_treinamento'](),
@@ -87,13 +107,13 @@ app.get('./treinamentos_disponiveis', (req, res) => {
                         }
                     );
                 }
-                
+
             }
         );
 
     });
 
-});
+});*/
 
 // módulo de sensor de cartão posta aqui para avisar que o status da raia mudou
 // (o usuário colocou ou retirou cartão)
